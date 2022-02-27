a38cee6043837eda11e170ec0d383efa5cfbb92e
Jakub Sachajko <37691739+eska5@users.noreply.github.com>
2022-02-28 00:26:09 +0100

    java Backend Version 4 Final
    Changed byte[] array to string which makes storage of photos in base64 a lot easier.

---
+++ javaBackend/src/main/java/Prog/MainProgram/MealPack/entity/meal.java
@@ -0,0 +1,26 @@
+package Prog.MainProgram.MealPack.entity;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "meals")
+
+public class meal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private int mealId;
+    private String mealName;
+    @Lob @Basic(fetch =FetchType.LAZY)
+    @Column(length=1000000)
+    @ToString.Exclude
+    private String mealPhoto;
+
+}
