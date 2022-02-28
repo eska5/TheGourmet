e4dfeaa48b0f5f934bb32948cac7122eb1823272
Łukasz Niedźwiadek <luki.niedzwiadek@gmail.com>
2022-02-28 12:56:40 +0100

    Version 3.3 of Gourmet WebApp
    File reorganization

---
+++ reactFrontend/src/Pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainSection from '../Elements/MainSection';
+
+const Home = () => {
+    
+    return (
+        <>
+        {/* dać credity dla autra iconki z flaticon.com */}
+        <MainSection />
+        </>
+    )
+}
+
+export default Home
