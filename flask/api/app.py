baad541894bcbabbaacc78a7a6e3ba90bdbf547a
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-03-07 21:48:29 +0100

    Version 3.5 of Gourmet WebApp
    Python is my friend, java spring is no more
    Python flask api finally works. Basic file catalog.

--- flask/api/app.py
+++ flask/api/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, redirect, url_for, request
 from flask_cors import CORS, cross_origin
+import dictionary as meal_dict
+import directoryHandler as dir
 app = Flask(__name__)
 cors = CORS(app, resources={r"/meals": {"origins": "http://localhost:3000"}})
+
 @app.route("/meals", methods=["POST"], strict_slashes=False)
 def meals():
     title = request.json['mealName']
-    body = request.json['mealPhoto']
-    #print(title)
-    print("test")
+    photo = request.json['mealPhoto']
+    meal_dict.MealsIndex(title)
     msg = "flask received photo with this name " + title;
     return msg
 
