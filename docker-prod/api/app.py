450e295d4a5a80015b865d4cdba50158da2ef6f7
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-03-10 10:38:44 +0100

    Prepare for deployment

---
+++ docker-prod/api/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect, url_for, request
+from flask_cors import CORS, cross_origin
+import directoryHandler as dirHandler
+app = Flask(__name__)
+cors = CORS(app, resources={r"/meals": {"origins": "http://localhost"}})
+
+@app.route("/meals", methods=["POST"], strict_slashes=False)
+def meals():
+    title = request.json['mealName']
+    photo = request.json['mealPhoto']
+    dirHandler.SaveAndDecodeMessage(title,photo)
+    #msg = "flask received photo with this name " + title
+    response = dirHandler.mealsList()
+    return response
+
+if __name__ == '__main__':
+   app.run(debug = True)
