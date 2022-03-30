97065f45ff621a7bb563f839b5ce162865fb2e74
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-03-30 15:41:18 +0200

    Version 2.0 of Gourmet deep learning script

--- modelTraining/network.py
+++ modelTraining/network.py
@@ -2,61 +2,72 @@ import tensorflow as tf
 import wandb
 from wandb.keras import WandbCallback
 from pathlib import Path
-import os.path
+import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-#wandb.init(project = "ImageRecognition")
+wandb.init(project="Food101", entity="gourmet")
 
-#print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+print("Num GPUs Available: ", len(tf.config.list_physical_devices("GPU")))
 
-train_dir = Path('C:\\Users\\Kuba\\Desktop\\zdjotka\\train')
-train_filepaths = list(train_dir.glob(r'**/*.jpg'))
+tf.config.threading.set_inter_op_parallelism_threads(0)
+tf.config.threading.set_intra_op_parallelism_threads(0)
 
-test_dir = Path('C:\\Users\\Kuba\\Desktop\\zdjotka\\test')
-test_filepaths = list(test_dir.glob(r'**/*.jpg'))
+train_dir = Path("D:\\Politechnika\\FoodDataSets\\Food101prepared\\train")
+train_filepaths = list(train_dir.glob(r"**/*.jpg"))
 
-val_dir = Path('C:\\Users\\Kuba\\Desktop\\zdjotka\\validation')
-val_filepaths = list(test_dir.glob(r'**/*.jpg'))
+test_dir = Path("D:\\Politechnika\\FoodDataSets\\Food101prepared\\test")
+test_filepaths = list(test_dir.glob(r"**/*.jpg"))
+
+val_dir = Path("D:\\Politechnika\\FoodDataSets\\Food101prepared\\validation")
+val_filepaths = list(val_dir.glob(r"**/*.jpg"))
+
 
 def proc_img(filepath):
     # dla linuxa inny slash split
-    labels = [str(filepath[i]).split("\\")[-2] \
-         for i in range(len(filepath))]
+    labels = [str(filepath[i]).split("\\")[-2] for i in range(len(filepath))]
 
-    filepath = pd.Series(filepath, name='Filepath').astype(str)
-    labels = pd.Series(labels, name='Label')
+    filepath = pd.Series(filepath, name="Filepath").astype(str)
+    labels = pd.Series(labels, name="Label")
 
     # Concatenate filepaths and labels
     df = pd.concat([filepath, labels], axis=1)
 
     # Shuffle the DataFrame and reset index
-    df = df.sample(frac=1).reset_index(drop = True)
-    
+    df = df.sample(frac=1).reset_index(drop=True)
+
     return df
+
 
 train_df = proc_img(train_filepaths)
 test_df = proc_img(test_filepaths)
 val_df = proc_img(val_filepaths)
 
-print('-- Training set --\n')
-print(f'Number of pictures: {train_df.shape[0]}\n')
-print(f'Number of different labels: {len(train_df.Label.unique())}\n')
-print(f'Labels: {train_df.Label.unique()}')
+print("-- Training set --\n")
+print(f"Number of pictures: {train_df.shape[0]}\n")
+print(f"Number of different labels: {len(train_df.Label.unique())}\n")
 
+print("-- Test set --\n")
+print(f"Number of pictures: {test_df.shape[0]}\n")
+print(f"Number of different labels: {len(test_df.Label.unique())}\n")
 
+print("-- Validation set --\n")
+print(f"Number of pictures: {val_df.shape[0]}\n")
+print(f"Number of different labels: {len(val_df.Label.unique())}\n")
+print(f"Labels: {val_df.Label.unique()}")
 
 # Create a DataFrame with one Label of each category
 df_unique = train_df.copy().drop_duplicates(subset=["Label"]).reset_index()
 
 # Display some pictures of the dataset
-fig, axes = plt.subplots(nrows=6, ncols=6, figsize=(8, 7),
-                        subplot_kw={'xticks': [], 'yticks': []})
+fig, axes = plt.subplots(
+    nrows=6, ncols=6, figsize=(8, 7), subplot_kw={"xticks": [], "yticks": []}
+)
 
 for i, ax in enumerate(axes.flat):
     ax.imshow(plt.imread(df_unique.Filepath[i]))
-    ax.set_title(df_unique.Label[i], fontsize = 12)
+    ax.set_title(df_unique.Label[i], fontsize=12)
 plt.tight_layout(pad=0.5)
 plt.show()
 
@@ -70,11 +81,11 @@ test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
 
 train_images = train_generator.flow_from_dataframe(
     dataframe=train_df,
-    x_col='Filepath',
-    y_col='Label',
-    target_size=(400, 400),
-    color_mode='rgb',
-    class_mode='categorical',
+    x_col="Filepath",
+    y_col="Label",
+    target_size=(300, 300),
+    color_mode="rgb",
+    class_mode="categorical",
     batch_size=32,
     shuffle=True,
     seed=0,
@@ -84,16 +95,16 @@ train_images = train_generator.flow_from_dataframe(
     height_shift_range=0.2,
     shear_range=0.15,
     horizontal_flip=True,
-    fill_mode="nearest"
+    fill_mode="nearest",
 )
 
 val_images = train_generator.flow_from_dataframe(
     dataframe=val_df,
-    x_col='Filepath',
-    y_col='Label',
-    target_size=(400, 400),
-    color_mode='rgb',
-    class_mode='categorical',
+    x_col="Filepath",
+    y_col="Label",
+    target_size=(300, 300),
+    color_mode="rgb",
+    class_mode="categorical",
     batch_size=32,
     shuffle=True,
     seed=0,
@@ -103,56 +114,50 @@ val_images = train_generator.flow_from_dataframe(
     height_shift_range=0.2,
     shear_range=0.15,
     horizontal_flip=True,
-    fill_mode="nearest"
+    fill_mode="nearest",
 )
 
 test_images = test_generator.flow_from_dataframe(
     dataframe=test_df,
-    x_col='Filepath',
-    y_col='Label',
-    target_size=(224, 224),
-    color_mode='rgb',
-    class_mode='categorical',
+    x_col="Filepath",
+    y_col="Label",
+    target_size=(300, 300),
+    color_mode="rgb",
+    class_mode="categorical",
     batch_size=32,
-    shuffle=False
+    shuffle=False,
 )
 
 # Load the pretained model
 pretrained_model = tf.keras.applications.MobileNetV2(
-    input_shape=(400, 400, 3),
+    input_shape=(300, 300, 3),
     include_top=False,
-    weights='imagenet',
-    pooling='avg'
+    weights="imagenet",
+    alpha=1.0,
+    input_tensor=None,
+    pooling="avg",
+    classes=101,
+    classifier_activation="softmax",
 )
 pretrained_model.trainable = False
 
-#Train
+# Train
 
 inputs = pretrained_model.input
 
-x = tf.keras.layers.Dense(128, activation='relu')(pretrained_model.output)
-x = tf.keras.layers.Dense(128, activation='relu')(x)
+x = tf.keras.layers.Dense(128, activation="relu")(pretrained_model.output)
+x = tf.keras.layers.Dense(128, activation="relu")(x)
 
-outputs = tf.keras.layers.Dense(36, activation='softmax')(x)
+outputs = tf.keras.layers.Dense(101, activation="softmax")(x)
 
 model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
-model.compile(
-    optimizer='adam',
-    loss='categorical_crossentropy',
-    metrics=['accuracy']
-)
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
 history = model.fit(
     train_images,
     validation_data=val_images,
-    batch_size = 32,
-    epochs=5,
-    callbacks= [
-        tf.keras.callbacks.EarlyStopping(
-            monitor='val_loss',
-            patience=2,
-            restore_best_weights=True
-        )
-        ]
+    batch_size=64,  # jak to wpływa na proces uczenia?
+    epochs=30,
+    callbacks=[WandbCallback()],
 )
