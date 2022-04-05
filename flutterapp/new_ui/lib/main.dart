de7fdf785176f8ade19ac2f3ba9851fca785ea1b
Łukasz Niedźwiadek <luki.niedzwiadek@gmail.com>
2022-04-05 12:30:09 +0200

    BottomNavBar is responsive
    there is no proof that this feature works :smiley:

--- flutterapp/new_ui/lib/main.dart
+++ flutterapp/new_ui/lib/main.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'dart:ui';
+
+bool smallSreen()
+    {
+      var pixelRatio = window.devicePixelRatio;
+      //Size in physical pixels
+      var physicalScreenSize = window.physicalSize;
+      var physicalWidth = physicalScreenSize.width;
+      var physicalHeight = physicalScreenSize.height;
+
+      //Size in logical pixels
+      var logicalScreenSize = window.physicalSize / pixelRatio;
+      var logicalWidth = logicalScreenSize.width;
+      var logicalHeight = logicalScreenSize.height;
+
+      if (physicalWidth < 720){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
 
 void main() => runApp(const MyApp());
 
@@ -8,7 +30,6 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   static const String _title = 'Flutter Code Sample';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -88,33 +109,60 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      bottomNavigationBar: BottomNavigationBar(
+      bottomNavigationBar: smallSreen() ? BottomNavigationBar(
         backgroundColor: Colors.indigo,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-            backgroundColor: Colors.indigo,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calculate_rounded),
-            label: 'Rozpoznaj potrawę',
-            backgroundColor: Colors.indigo,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_photo_alternate_rounded),
-            label: 'Dodaj potrawę',
-            backgroundColor: Colors.indigo,
-          ),
+        items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+          backgroundColor: Colors.indigo,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calculate_rounded),
+          label: 'Rozpoznaj potrawę',
+          backgroundColor: Colors.indigo,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_photo_alternate_rounded),
+          label: 'Dodaj potrawę',
+          backgroundColor: Colors.indigo,
+        )
+        ],
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.indigo[50],
+        onTap: _onItemTapped,
+      ) 
+      :
+      BottomNavigationBar(
+        backgroundColor: Colors.indigo,
+        items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+          backgroundColor: Colors.indigo,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calculate_rounded),
+          label: 'Rozpoznaj potrawę',
+          backgroundColor: Colors.indigo,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_photo_alternate_rounded),
+          label: 'Dodaj potrawę',
+          backgroundColor: Colors.indigo,
+        )
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.indigo[50],
         onTap: _onItemTapped,
-      ),
+      )
     );
   }
 }
