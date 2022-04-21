20f6ccad48e07dbd73ee2b0dcd9c2a297e1f3a3b
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-04-21 21:48:11 +0200

    Classify and Add has now HTTPS post methods

--- FlutterApp/flutterapp/new_ui/lib/main.dart
+++ FlutterApp/flutterapp/new_ui/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:new_ui/screens/add.dart';
 import 'package:new_ui/screens/classify.dart';
 import 'package:new_ui/screens/home.dart';
