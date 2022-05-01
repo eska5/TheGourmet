6d5f7d2a58c60de8468c9dc4ccb6d03bcd87aa9f
Jakub Sachajko <kuba.sachajko@gmail.com>
2022-05-02 00:17:46 +0200

    Add Ui improvements
    Added popup in add menu.
    Added back button in suggestions.
    Updated info ui.

--- flutterapp/flutterapp/new_ui/lib/popupcard/add_todo_button.dart
+++ flutterapp/flutterapp/new_ui/lib/popupcard/add_todo_button.dart
@@ -3,7 +3,6 @@ import 'package:new_ui/popupcard/custom_rect_tween.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:new_ui/components/button.dart';
 
-
 import 'custom_rect_tween.dart';
 import 'hero_dialog_route.dart';
 
@@ -21,7 +20,7 @@ class AddTodoButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 32,right: 32, top: 32, bottom: 32),
+      padding: const EdgeInsets.only(left: 32, right: 32, top: 32, bottom: 32),
       child: GestureDetector(
         onTap: () {
           Navigator.of(context).push(HeroDialogRoute(builder: (context) {
@@ -34,29 +33,30 @@ class AddTodoButton extends StatelessWidget {
             return CustomRectTween(begin: begin!, end: end!);
           },
           child: Material(
-            color: Colors.indigo,
-            shadowColor: Colors.blue,
-            elevation: 2,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
-            child: SizedBox(
-              width: 190, // <-- Your width
-              height: 55, // <-- Your height
-              child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              const Icon(Icons.info_outline_rounded, size: 28,color: Colors.white),
-              const Text(" Instrukcja",
-              style: TextStyle(
-                fontSize: 25,
-                letterSpacing: 0,
-                color: Colors.white,
-              ),
-              ),
-            ],)
-            )
-          ),
+              color: Colors.indigo,
+              shadowColor: Colors.blue,
+              elevation: 2,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32)),
+              child: SizedBox(
+                  width: 190, // <-- Your width
+                  height: 55, // <-- Your height
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(Icons.info_outline_rounded,
+                          size: 28, color: Colors.white),
+                      const Text(
+                        " Instrukcja",
+                        style: TextStyle(
+                          fontSize: 25,
+                          letterSpacing: 0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ))),
         ),
       ),
     );
@@ -80,7 +80,8 @@ class _AddTodoPopupCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Padding(
-        padding: const EdgeInsets.only(left: 32,right: 32, top: 80, bottom: 100),
+        padding:
+            const EdgeInsets.only(left: 32, right: 32, top: 80, bottom: 100),
         child: Hero(
           tag: _heroAddTodo,
           createRectTween: (begin, end) {
@@ -93,94 +94,117 @@ class _AddTodoPopupCard extends StatelessWidget {
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
             child: SingleChildScrollView(
               child: Padding(
-                padding: const EdgeInsets.only(left: 18,right: 18, top: 16, bottom: 18),
+                padding: const EdgeInsets.only(
+                    left: 18, right: 18, top: 16, bottom: 18),
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   //crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
                     Padding(
-                padding: EdgeInsets.only(left: 15, right: 15, top: 20, bottom:20), //apply padding to all four sides
-                    child :Text("O projekcie",style: GoogleFonts.comfortaa(
-                    fontSize: 32,
-                    textStyle: TextStyle(
-                    letterSpacing: 0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white ),
-                  )),
-                  ),
+                      padding: EdgeInsets.only(
+                          left: 15,
+                          right: 15,
+                          top: 20,
+                          bottom: 20), //apply padding to all four sides
+                      child: Text("O projekcie",
+                          style: GoogleFonts.comfortaa(
+                            fontSize: 32,
+                            textStyle: TextStyle(
+                                letterSpacing: 0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          )),
+                    ),
                     Padding(
-                      padding: EdgeInsets.only(left: 15, right: 15, top: 10, bottom: 20), //apply padding to all four sides
+                      padding: EdgeInsets.only(
+                          left: 15,
+                          right: 15,
+                          top: 10,
+                          bottom: 20), //apply padding to all four sides
                       child: Text(
-                        "Celem aplikacji jest zbieranie podpisanych zdjęć żywności, które potem użyjemy do uczenia modelu sieci neuronowej, której zadaniem będzie rozpoznawanie jedzenia na podstwie zdjęć. Dziękujemy za wkład i poświęcony czas.",style: GoogleFonts.comfortaa(
-                      fontSize: 18,
-                      textStyle: TextStyle(
-                        letterSpacing: 0,
-                        fontWeight: FontWeight.bold,
-                    color: Colors.white  ),
-                    )),
+                          "Celem aplikacji jest zbieranie podpisanych zdjęć żywności, które potem użyjemy do uczenia modelu sieci neuronowej, której zadaniem będzie rozpoznawanie jedzenia na podstwie zdjęć. Dziękujemy za wkład i poświęcony czas.",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.comfortaa(
+                            fontSize: 18,
+                            textStyle: TextStyle(
+                                letterSpacing: 0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          )),
                     ),
                     const Divider(
                       color: Colors.white,
                       thickness: 0.2,
                     ),
                     Padding(
-                padding: EdgeInsets.only(left: 15, right: 15, top: 10, bottom: 20), //apply padding to all four sides
-                child: Text("Instrukcja",style: GoogleFonts.comfortaa(
-                fontSize: 32,
-                textStyle: TextStyle(
-                  letterSpacing: 0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white ),
-              )),
-              ),
-               Padding(
-                padding: EdgeInsets.only(left: 15, right: 15, top: 10, bottom: 20), //apply padding to all four sides
-                child: Text(
-                  "W podstronie \"Dodaj potrawę\", znajduję się przycisk \"Wybierz zdjęcie\", użyj go aby wybrać zdjęcie z galerii albo zrobić je aparatem. Następnie podpisz zdjęcie używając pola do wprowadzania tekstu. Jeśli nazwa potrawy, którą chcesz wprowadzić znajduję się w panelu sugestii, skorzystaj z podpowiedzi. Po wprowadzeniu nazwy i obrazka, kliknij przycisk wyślij.",
-                  style: GoogleFonts.comfortaa(
-                fontSize: 18,
-                textStyle: TextStyle(
-                  letterSpacing: 0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white ),
-              )),
-              ),
-
-                            const Divider(
-                  color: Colors.white,
-                  thickness: 0.2,
-                ),
-
-                Padding(
-                padding: EdgeInsets.only(left: 15, right: 15, top: 15, bottom: 15), //apply padding to all four sides
-                child: SizedBox(
-                    width: 235, // <-- Your width
-                    height: 60, // <-- Your height
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.white,
-                        onPrimary: Colors.indigo,
-
-                        textStyle: TextStyle(fontSize: 20),
-                        shape:
-                            RoundedRectangleBorder(borderRadius: BorderRadius.circular(32.0)),
-                        //minimumSize: const Size(40, 60),
-                      ),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.exit_to_app_outlined, size: 28),
-                          const SizedBox(width: 10),
-                          Text("Powrót"),
-                        ],
-                      ),
-                      onPressed: () =>  Navigator.pop(context, _AddTodoPopupCard()),
+                      padding: EdgeInsets.only(
+                          left: 15,
+                          right: 15,
+                          top: 10,
+                          bottom: 20), //apply padding to all four sides
+                      child: Text("Instrukcja",
+                          style: GoogleFonts.comfortaa(
+                            fontSize: 32,
+                            textStyle: TextStyle(
+                                letterSpacing: 0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          )),
                     ),
-                  ),
-                )
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: 15,
+                          right: 15,
+                          top: 10,
+                          bottom: 20), //apply padding to all four sides
+                      child: Text(
+                          "W podstronie \"Dodaj potrawę\", znajduję się przycisk \"Wybierz zdjęcie\", użyj go aby wybrać zdjęcie z galerii albo zrobić je aparatem. Następnie podpisz zdjęcie używając pola do wprowadzania tekstu. Jeśli nazwa potrawy, którą chcesz wprowadzić znajduję się w panelu sugestii, skorzystaj z podpowiedzi. Po wprowadzeniu nazwy i obrazka, kliknij przycisk wyślij.",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.comfortaa(
+                            fontSize: 18,
+                            textStyle: TextStyle(
+                                letterSpacing: 0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          )),
+                    ),
+                    const Divider(
+                      color: Colors.white,
+                      thickness: 0.2,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: 15,
+                          right: 15,
+                          top: 15,
+                          bottom: 15), //apply padding to all four sides
+                      child: SizedBox(
+                        width: 235, // <-- Your width
+                        height: 60, // <-- Your height
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.white,
+                            onPrimary: Colors.indigo,
 
-
+                            textStyle: TextStyle(fontSize: 20),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(32.0)),
+                            //minimumSize: const Size(40, 60),
+                          ),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.exit_to_app_outlined, size: 28),
+                              const SizedBox(width: 10),
+                              Text("Powrót"),
+                            ],
+                          ),
+                          onPressed: () =>
+                              Navigator.pop(context, _AddTodoPopupCard()),
+                        ),
+                      ),
+                    )
                   ],
                 ),
               ),
