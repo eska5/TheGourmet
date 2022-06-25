c72e17be8d543e04ea31a3a5a447948d9db4c258
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-06-25 13:36:23 +0200

    draft of finding document in database

--- meals_operator/app.py
+++ meals_operator/app.py
@@ -1,3 +1,6 @@
+import json
+
+import requests
 from flask import Flask, request
 from flask_cors import CORS
 
@@ -28,6 +31,34 @@ def bad_result():
     dir_name = f"{request.json['modeloutput']}_{request.json['useroutput']}"
     m_operator.save_image(dir_name, request.json["mealPhoto"])
     return "OK"
+
+
+@app.route("/details", methods=["GET"], strict_slashes=False)
+def meal_details():
+    meal_name = "marchewka"
+    url = "https://data.mongodb-api.com/app/data-bduvb/endpoint/data/v1/action/findOne"
+    payload = json.dumps({
+        "collection": "meals",
+        "database": "gourmet",
+        "dataSource": "Cluster0",
+        "filter": {"name": meal_name},
+        "projection": {
+            "_id": 1,
+            "name": 1,
+            "calories": 1,
+            "alergens": 1,
+            "photos": 1,
+
+        }
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Request-Headers': '*',
+        'api-key': '4wyTSiqX9oBUrS8o3X9WnSAwifMFmXfa1DdO39ElkY3WuxjAkOQcUExbDtSXzWJ7',
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(response.text)
+    return response.text
 
 
 if __name__ == "__main__":
