7f4f4340b26079ebf6d051ac94ad9567fc575e82
Jakub Sachajko <kuba.sachajko@gmail.com>
2022-06-30 19:22:35 +0200

    PI-190 downgraded dependency

--- FlutterApp/lib/main.dart
+++ FlutterApp/lib/main.dart
@@ -1,4 +1,3 @@
-// @dart=2.9
 import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
@@ -16,7 +15,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   static const String _title = 'Gourmet';
 
@@ -37,7 +36,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key key}) : super(key: key);
+  const MyStatefulWidget({Key? key}) : super(key: key);
 
   @override
   State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
