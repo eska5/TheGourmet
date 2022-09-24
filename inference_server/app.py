1cc5e44b98ceeba2ae6717d096eff4ac8919807f
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-09-24 22:17:23 +0200

    code refactor and response_type.py update

--- inference_server/app.py
+++ inference_server/app.py
@@ -26,19 +26,18 @@ logging.basicConfig(
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
+with open("descriptions.yaml", 'r', encoding='utf8') as stream:
+    descriptions = yaml.safe_load(stream)['descriptions']
+
+labels = []
+for key in descriptions.keys():
+    labels.append(key)
+
 
 @app.before_first_request
 def before_first_request():
     global model
     model = tensorflow.keras.models.load_model("model.h5")
-
-    global labels
-    with open("catalog.yaml", 'r', encoding='utf8') as stream:
-        labels = yaml.safe_load(stream)['catalog']
-
-    global descriptions
-    with open("descriptions.yaml", 'r', encoding='utf8') as stream:
-        descriptions = yaml.safe_load(stream)['descriptions']
 
 
 @app.route("/ping", methods=["GET"], strict_slashes=False)
@@ -52,7 +51,11 @@ def app_health_check():
 
 @app.route("/catalog", methods=["GET"], strict_slashes=False)
 def meals_catalog():
-    return app.response_class(response=json.dumps(labels).encode('utf8'),
+    app.logger.info("Creating meal catalog...")
+    catalog = []
+    for meal in labels:
+        catalog.append(descriptions[meal]["name"])
+    return app.response_class(response=json.dumps(catalog).encode('utf8'),
                               content_type='application/json')
 
 
@@ -73,8 +76,7 @@ def classify_photo():
         name = labels[predict.argmax(axis=1)[0]]
         certainty = predict[0][[predict.argmax(axis=1)[0]]][0]
         predict[0][[predict.argmax(axis=1)[0]]] = 0
-        predictions.append(
-            Meal(name=name, description=descriptions[name.lower()], certainty=certainty).to_dict())
+        predictions.append(Meal(certainty=certainty, description=descriptions[name.lower()]).to_dict())
     app.logger.info("Predictions created. Classification finished")
     return app.response_class(response=json.dumps(predictions).encode('utf8'),
                               content_type='application/json')
@@ -84,42 +86,39 @@ def classify_photo():
 def detect_meal():
     app.logger.info("Running YOLO classification on the image...")
 
-    # Load Image with PIL
+    app.logger.info("Loading image from base64")
     image_from_base64 = Image.open(BytesIO(base64.b64decode(str(request.json["img_for_detection"]))))
     numpy_coded_image = np.array(image_from_base64.convert("RGB"))
     image_for_inference = Image.fromarray(cv2.resize(numpy_coded_image, (400, 400)))
     final_image = cv2.cvtColor(cv2.resize(numpy_coded_image, (400, 400)), cv2.COLOR_BGR2RGB)
 
-    # Convert to JPEG Buffer
+    app.logger.info("Creating JPEG buffer")
     buffered = BytesIO()
     image_for_inference.save(buffered, quality=100, format="JPEG")
 
-    app.logger.info("Sending request to Roboflow...")
+    app.logger.info("Sending request to Roboflow")
     multipart_form = MultipartEncoder(fields={'file': ("imageToUpload", buffered.getvalue(), "image/jpeg")})
     roboflow_response = requests.post(f"{MODEL_URL}?api_key={MY_KEY}",
                                       data=multipart_form,
                                       headers={'Content-Type': multipart_form.content_type}).json()
 
-    print(roboflow_response)
-    # label list
-    app.logger.info("Creating predictions...")
+    app.logger.info(f"Response from Roboflow model: {roboflow_response}")
+
     predictions = []
     detection_result = []
     for prediction in roboflow_response["predictions"]:
         if prediction["class"] not in detection_result:
             detection_result.append(prediction["class"])
 
-    # initialize a list of colors to represent each possible class label
+    app.logger.info("Drawing result on the new image")
     np.random.seed(42)
     COLORS = np.random.randint(0, 255, size=(len(detection_result), 3),
                                dtype="uint8")
 
-    # ensure at least one detection exists
-    if len(roboflow_response["predictions"]) > 0:
+    if roboflow_response["predictions"]:
         for index, prediction in enumerate(roboflow_response["predictions"]):
             predictions.append(
-                Meal(name=prediction["class"], certainty=prediction["confidence"],
-                     description=descriptions[prediction["class"]]).to_dict())
+                Meal(certainty=prediction["confidence"], description=descriptions[prediction["class"]]).to_dict())
 
             (x1, y1) = (int(prediction["x"] - prediction['width'] / 2), int(prediction["y"] - prediction['height'] / 2))
             (x2, y2) = (int(prediction["x"] + prediction["width"] / 2), int(prediction["y"] + prediction["height"] / 2))
@@ -129,15 +128,11 @@ def detect_meal():
             cv2.putText(final_image, text, (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX,
                         0.5, color, 2)
 
-    image_debug = base64.b64encode(cv2.imencode('.jpg', final_image)[1]).decode()
+    app.logger.info(f"Inference results: {predictions}")
     yolo_image = {
-        'detection_result': image_debug
+        'detection_result': base64.b64encode(cv2.imencode('.jpg', final_image)[1]).decode()
     }
     predictions.insert(0, yolo_image)
-    imgdata = base64.b64decode(image_debug)
-    filename = 'some_image.jpg'  # I assume you have a way of picking unique filenames
-    with open(filename, 'wb') as f:
-        f.write(imgdata)
     app.logger.info("Detection finished.")
     return app.response_class(status=200, response=json.dumps(predictions).encode('utf8'),
                               content_type='application/json')
--- inference_server/response_type.py
+++ inference_server/response_type.py
@@ -1,8 +1,10 @@
 class Meal:
-    def __init__(self, name: str, description: str, certainty: float):
-        self.name = name
-        self.certainty = certainty
-        self.description = description
+    def __init__(self, description: dict, certainty: float):
+        self.name: str = description["name"]
+        self.calories: int = description["calories"]
+        self.allergens: list = description["allergens"]
+        self.certainty: float = certainty
 
     def to_dict(self) -> dict:
-        return {"name": self.name, "description": self.description, "certainty": str(self.certainty)}
+        return {"name": self.name, "certainty": str(self.certainty), "calories": self.calories,
+                "allergens": self.allergens}
