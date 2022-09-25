dd5dc3b59edcc641c860405d63ffcd51a60e991a
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-09-25 14:50:02 +0200

    detection result cards implemented

--- FlutterApp/lib/resources/classify_screen/classify_methods.dart
+++ FlutterApp/lib/resources/classify_screen/classify_methods.dart
@@ -12,12 +12,12 @@ import 'package:new_ui/resources/classify_screen/sub_screens/detection_results.d
 import '../../screens/report.dart';
 import '../common/snack_bars.dart';
 
-String responseBody = "";
-int cardIndex = 0;
+String classifyResponse = "";
+int classifyCardIndex = 0;
 
 void sendClassifyRequest(BuildContext context, Uint8List? bytes) async {
   ResultScreen.isClassified.value = false;
-  cardIndex = 0;
+  classifyCardIndex = 0;
   classifyResultCards = [
     CardDetails(color: Colors.blue.shade300, cardNumber: 1),
     CardDetails(color: Colors.blue.shade300, cardNumber: 2),
@@ -32,7 +32,7 @@ void sendClassifyRequest(BuildContext context, Uint8List? bytes) async {
     "Access-Control-Allow-Origin": "*"
   };
 
-  Map<String, dynamic> body = {'mealPhoto': base64Encode(bytes!)};
+  Map<String, dynamic> body = {'image': base64Encode(bytes!)};
   String jsonBody = json.encode(body);
   final encoding = Encoding.getByName('utf-8');
 
@@ -45,8 +45,8 @@ void sendClassifyRequest(BuildContext context, Uint8List? bytes) async {
           encoding: encoding,
         )
         .timeout(const Duration(seconds: 30));
-    responseBody = response.body;
-    setDataInCards(3);
+    classifyResponse = response.body;
+    setDataInCards(3, true, response.body);
     ResultScreen.isClassified.value = true;
     if (kDebugMode) {
       print(ResultScreen.isClassified.value);
@@ -62,7 +62,9 @@ void sendClassifyRequest(BuildContext context, Uint8List? bytes) async {
 
 void sendDetectionRequest(BuildContext context, Uint8List? bytes) async {
   ResultScreen.isDetected.value = false;
-  detectionResultCards = [];
+  detectionResultCards = [
+    CardDetails(color: Colors.blue.shade300, cardNumber: 1)
+  ];
   DetectionResultScreen.detectedImage = null;
   if (kDebugMode) {
     print(ResultScreen.isClassified.value);
@@ -73,7 +75,7 @@ void sendDetectionRequest(BuildContext context, Uint8List? bytes) async {
     "Access-Control-Allow-Origin": "*"
   };
 
-  Map<String, dynamic> body = {'img_for_detection': base64Encode(bytes!)};
+  Map<String, dynamic> body = {'image': base64Encode(bytes!)};
   String jsonBody = json.encode(body);
   final encoding = Encoding.getByName('utf-8');
 
@@ -86,15 +88,15 @@ void sendDetectionRequest(BuildContext context, Uint8List? bytes) async {
           encoding: encoding,
         )
         .timeout(const Duration(seconds: 30));
-    responseBody = response.body;
     ResultScreen.isDetected.value = true;
-    int detectedMeals = json.decode(responseBody).length - 1;
-    for (int i = 0; i < detectedMeals; i++) {
+    int detectedMeals = json.decode(response.body).length - 1;
+    for (int i = 1; i < detectedMeals; i++) {
       detectionResultCards
           .add(CardDetails(color: Colors.blue.shade300, cardNumber: i + 1));
     }
-    DetectionResultScreen.detectedImage = base64Decode(
-        Map.from(json.decode(responseBody)[0])["detection_result"]);
+    DetectionResultScreen.detectedImage =
+        base64Decode(Map.from(json.decode(response.body)[0])["result_image"]);
+    setDataInCards(detectedMeals, false, response.body);
     if (kDebugMode) {
       print("detection performed");
     }
@@ -110,34 +112,42 @@ void navigateToReportScreen(BuildContext context) async {
   );
 }
 
-void setDataInCards(int amount) {
-  for (int i = cardIndex; i < cardIndex + amount; i++) {
-    classifyResultCards[i].mealName =
-        Map.from(json.decode(responseBody)[i])["name"];
-    classifyResultCards[i].mealProbability =
-        double.parse(Map.from(json.decode(responseBody)[i])["certainty"]) * 100;
-
-    if (Map.from(json.decode(responseBody)[i])["description"] !=
-        "Brak danych") {
-      Map description =
-          Map.from(Map.from(json.decode(responseBody)[i])["description"]);
+void setDataInCards(int amount, bool classification, String data) {
+  if (classification) {
+    for (int i = classifyCardIndex; i < classifyCardIndex + amount; i++) {
+      classifyResultCards[i].mealName = Map.from(json.decode(data)[i])["name"];
+      classifyResultCards[i].mealProbability =
+          double.parse(Map.from(json.decode(data)[i])["certainty"]) * 100;
+      var mealCalories = Map.from(json.decode(data)[i])["calories"];
+      var mealAllergens = Map.from(json.decode(data)[i])["allergens"];
       classifyResultCards[i].mealDescription =
-          "Szacowane kalorie na 100g: ${description['calories']} kcal\nMożliwe alergeny: ${description['allergens'].toString().replaceAll(RegExp(r'[\[\]]'), '')}";
-    } else {
-      classifyResultCards[i].mealDescription =
-          Map.from(json.decode(responseBody)[i])["description"];
+          "Szacowane kalorie na 100g: ${mealCalories} kcal\nMożliwe alergeny: ${mealAllergens.toString().replaceAll(RegExp(r'[\[\]]'), '')}";
+    }
+    classifyCardIndex += amount;
+  } else {
+    if (amount == 0) {
+      detectionResultCards[0].mealName = "Nie wykryto potrawy";
+    }
+    for (int i = 0; i < amount; i++) {
+      detectionResultCards[i].mealName =
+          Map.from(json.decode(data)[i + 1])["name"];
+      detectionResultCards[i].mealProbability =
+          double.parse(Map.from(json.decode(data)[i + 1])["certainty"]) * 100;
+      var mealCalories = Map.from(json.decode(data)[i + 1])["calories"];
+      var mealAllergens = Map.from(json.decode(data)[i + 1])["allergens"];
+      detectionResultCards[i].mealDescription =
+          "Szacowane kalorie na 100g: ${mealCalories} kcal\nMożliwe alergeny: ${mealAllergens.toString().replaceAll(RegExp(r'[\[\]]'), '')}";
     }
   }
-  cardIndex += amount;
 }
 
 void getMoreResults(int amount, Function onClick, BuildContext context) {
-  if (cardIndex < 7) {
-    for (int i = cardIndex; i < cardIndex + amount; i++) {
+  if (classifyCardIndex < 7) {
+    for (int i = classifyCardIndex; i < classifyCardIndex + amount; i++) {
       classifyResultCards
           .add(CardDetails(color: Colors.blue.shade300, cardNumber: 4));
     }
-    setDataInCards(amount);
+    setDataInCards(amount, true, classifyResponse);
     onClick();
   } else {
     showErrorMessage(context, "Osiągnięto limit propozycji");
