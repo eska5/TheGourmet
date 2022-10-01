b4350285bec6fd355bab9649dbf08a93825d588b
Jakub Sachajko <kuba.sachajko@gmail.com>
2022-10-01 15:07:19 +0200

    Update PrepareData.py

--- training_scripts/modelTraining/PrepareData.py
+++ training_scripts/modelTraining/PrepareData.py
@@ -4,27 +4,28 @@ from PIL import Image
 import numpy as np
 
 
-categories = []
-pathTrain = "C:\\Users\\kubas\\OneDrive\\Desktop\\DATA\\train\\"
-pathValidation = "C:\\Users\\kubas\\OneDrive\\Desktop\\DATA\\validation\\"
-pathTest = "C:\\Users\\kubas\\OneDrive\\Desktop\\DATA\\test\\"
+CATEGORIES = []
+PATH_TRAINING = "C:\\Users\\kubas\\OneDrive\\Desktop\\DATA\\train\\"
+PATH_VALIDATION = "C:\\Users\\kubas\\OneDrive\\Desktop\\DATA\\validation\\"
+PATH_TEST = "C:\\Users\\kubas\\OneDrive\\Desktop\\DATA\\test\\"
 
 try:
     os.mkdir("C:\\Users\\kubas\\OneDrive\\Desktop\\DATA\\convertedTrain")
 except:
     pass
 
-for category in os.listdir(pathTrain):
-    categories.append(category)
-    imagesPath = os.path.join(pathTrain, category)
+for category in os.listdir(PATH_TRAINING):
+    CATEGORIES.append(category)
+    imagesPath = os.path.join(PATH_TRAINING, category)
     print(imagesPath)
     try:
         os.mkdir("C:\\Users\\kubas\\OneDrive\\Desktop\\DATA\\convertedTrain\\" + category)
-    except:
+    except Exception as e:
+        print(e)
         pass
     for imgPath in os.listdir(imagesPath):
         print(imgPath)
-        img = Image.open(pathTrain + category + "\\" + imgPath).convert("RGB")
+        img = Image.open(PATH_TRAINING + category + "\\" + imgPath).convert("RGB")
         opencvImage = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
         im = cv2.resize(opencvImage, (400, 400))
         cv2.imwrite(
@@ -33,20 +34,20 @@ for category in os.listdir(pathTrain):
 
 try:
     os.mkdir("C:\\Users\\kubas\\OneDrive\\Desktop\\DATA\\convertedValidation")
-except:
-    pass
-for category in os.listdir(pathValidation):
-    categories.append(category)
-    imagesPath = os.path.join(pathValidation, category)
+except Exception as e:
+    print(e)
+for category in os.listdir(PATH_VALIDATION):
+    CATEGORIES.append(category)
+    imagesPath = os.path.join(PATH_VALIDATION, category)
     print(imagesPath)
     try:
         os.mkdir("C:\\Users\\kubas\\OneDrive\\Desktop\\DATA\\convertedValidation\\" + category)
-    except:
-        pass
+    except Exception as e:
+        print(e)
     for imgPath in os.listdir(imagesPath):
         print(imgPath)
         try:
-            img = Image.open(pathValidation + category + "\\" + imgPath).convert("RGB")
+            img = Image.open(PATH_VALIDATION + category + "\\" + imgPath).convert("RGB")
             opencvImage = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
             im = cv2.resize(opencvImage, (400, 400))
             cv2.imwrite(
@@ -54,24 +55,24 @@ for category in os.listdir(pathValidation):
                 im,
             )
         except Exception as e:
-            pass
+            print(e)
 
 try:
     os.mkdir("C:\\Users\\kubas\\OneDrive\\Desktop\\DATA\\convertedTest")
-except:
-    pass
-for category in os.listdir(pathTest):
-    categories.append(category)
-    imagesPath = os.path.join(pathTest, category)
+except Exception as e:
+    print(e)
+for category in os.listdir(PATH_TEST):
+    CATEGORIES.append(category)
+    imagesPath = os.path.join(PATH_TEST, category)
     print(imagesPath)
     try:
         os.mkdir("C:\\Users\\kubas\\OneDrive\\Desktop\\DATA\\convertedTest\\" + category)
-    except:
-        pass
+    except Exception as e:
+        print(e)
     for imgPath in os.listdir(imagesPath):
         print(imgPath)
         try:
-            img = Image.open(pathTest + category + "\\" + imgPath).convert("RGB")
+            img = Image.open(PATH_TEST + category + "\\" + imgPath).convert("RGB")
             opencvImage = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
             im = cv2.resize(opencvImage, (400, 400))
             cv2.imwrite(
