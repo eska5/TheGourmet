38927bfe1e16f99d036a787909a03473c59e50a2
Łukasz Niedźwiadek <luki.niedzwiadek@gmail.com>
2022-10-06 19:20:31 +0200

    style fix 2

--- src/meals_operator/tests/unit/test_app.py
+++ src/meals_operator/tests/unit/test_app.py
@@ -4,7 +4,7 @@ import meal_operator as m_operator
 
 
 @pytest.fixture
-def client():
+def test_client():
     app.config["TESTING"] = True
 
     with app.app_context():
@@ -12,28 +12,28 @@ def client():
             yield flask_client
 
 
-def test_app_health_check(client):
-    resp = client.get("/ping")
+def test_app_health_check(test_client):
+    resp = test_client.get("/ping")
     assert resp.status_code == 200
     assert str(resp.data)[2:-1] == "App is running."
     assert resp.content_type == "application/json"
 
 
-def test_suggestions(client, mocker):
+def test_suggestions(test_client, mocker):
     json_dumps_mock = mocker.patch("app.json.dumps")
     get_suggestions_mock = mocker.patch("app.m_operator.get_suggestions")
-    resp = client.get("/suggestions")
+    resp = test_client.get("/suggestions")
     get_suggestions_mock.assert_called_once()
     json_dumps_mock.assert_called_once()
     assert resp.status_code == 200
     assert resp.content_type == "application/json"
 
 
-def test_meals_success(client, mocker):
+def test_meals_success(test_client, mocker):
     example_name = "potato"
     example_photo = "potato_photo.jpg"
     save_image_mock = mocker.patch("app.m_operator.save_image")
-    resp = client.post(
+    resp = test_client.post(
         "/meals", json={"mealName": example_name, "mealPhoto": example_photo}
     )
     assert resp.status_code == 201
@@ -42,27 +42,27 @@ def test_meals_success(client, mocker):
     )
 
 
-def test_meals_fail(client):
+def test_meals_fail(test_client):
     example_name = ""
     example_photo = ""
-    resp = client.post(
+    resp = test_client.post(
         "/meals", json={"mealName": example_name, "mealPhoto": example_photo}
     )
     assert resp.status_code == 400
 
 
-def test_meals_fail_key_error(client):
-    resp = client.post("/meals", json={"data": "example_data"})
+def test_meals_fail_key_error(test_client):
+    resp = test_client.post("/meals", json={"data": "example_data"})
     assert resp.status_code == 400
 
 
-def test_bad_result(client, mocker):
+def test_bad_result(test_client, mocker):
     example_correct_name = "potato"
     example_bad_name = "banana"
     example_photo = "potato_photo.jpg"
     mock_bad_result_name = f"{example_bad_name}_{example_correct_name}"
     save_image_mock = mocker.patch("app.m_operator.save_image")
-    resp = client.post(
+    resp = test_client.post(
         "/badresult",
         json={
             "useroutput": example_correct_name,
@@ -78,16 +78,16 @@ def test_bad_result(client, mocker):
     )
 
 
-def test_bad_result_fail_key_error(client):
-    resp = client.post("/badresult", json={"data": "example_data"})
+def test_bad_result_fail_key_error(test_client):
+    resp = test_client.post("/badresult", json={"data": "example_data"})
     assert resp.status_code == 400
 
 
-def test_bad_result_fail(client):
+def test_bad_result_fail(test_client):
     example_correct_name = ""
     example_bad_name = ""
     example_photo = "potato_photo.jpg"
-    resp = client.post(
+    resp = test_client.post(
         "/badresult",
         json={
             "useroutput": example_correct_name,
@@ -98,17 +98,17 @@ def test_bad_result_fail(client):
     assert resp.status_code == 400
 
 
-def test_meal_details(client, mocker):
+def test_meal_details(test_client, mocker):
     example_name = "pizza"
     json_dumps_mock = mocker.patch("app.json.dumps")
     get_meal_mock = mocker.patch("app.m_operator.get_meal")
-    resp = client.get("/details", query_string={"name": example_name})
+    resp = test_client.get("/details", query_string={"name": example_name})
     get_meal_mock.assert_called_once_with(meal_name=example_name)
     json_dumps_mock.assert_called_once()
     assert resp.status_code == 200
     assert resp.content_type == "application/json"
 
 
-def test_meal_details_fail(client):
-    resp = client.get("/details")
+def test_meal_details_fail(test_client):
+    resp = test_client.get("/details")
     assert resp.status_code == 404
