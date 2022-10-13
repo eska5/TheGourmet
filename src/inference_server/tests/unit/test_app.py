6c35a85cb30b548e4468400f55c78e96ff1838df
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-10-13 15:45:47 +0200

    THIS VERSION WORKS

--- src/inference_server/tests/unit/test_app.py
+++ src/inference_server/tests/unit/test_app.py
@@ -15,17 +15,20 @@ def test_client():
 def test_catalog(test_client, mocker):
     catalog_mock = ["tomato", "potato", "pizza"]
 
-    meals_data_mock = mocker.patch("app.MealsData.get_catalog", return_value=catalog_mock)
+    meals_data_mock = mocker.patch(
+        "app.MealsData.get_catalog", return_value=catalog_mock
+    )
     resp = test_client.get("/catalog")
     assert resp.status_code == 200
     assert resp.text == '["tomato", "potato", "pizza"]'
     meals_data_mock.assert_called_once()
 
 
-@pytest.mark.parametrize("test_input,expected", [(None, "not loaded"), (MagicMock(), "loaded")])
+@pytest.mark.parametrize(
+    "test_input,expected", [(None, "not loaded"), (MagicMock(), "loaded")]
+)
 def test_ping(test_client, mocker, test_input, expected):
     mocker.patch("app.MODEL", test_input)
     resp = test_client.get("/ping")
     assert resp.status_code == 200
     assert resp.text == f"App is running. Model is {expected}."
-
